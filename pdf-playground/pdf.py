f4896ac53145edc5ced09e847c4a90a0f09cdc7d
Laykajenta <marianne.odegaard@outlook.com>
2025-07-21 20:02:56 +0200

    add vode to tilt an pdf

--- pdf-playground/pdf.py
+++ pdf-playground/pdf.py
@@ -1,1 +1,12 @@
-PDf whith python 
+import PyPDF2
+
+
+
+with open('dummy.pdf', 'rb') as file:
+    reader = PyPDF2.PdfReader(file)
+    side=reader.pages[0]
+    side.rotate(90)
+    writer = PyPDF2.PdfWriter()
+    writer.add_page(side)
+    with open('tilt.pdf', 'wb') as new_file:
+        writer.write(new_file)
